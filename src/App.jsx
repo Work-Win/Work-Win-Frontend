50769fcfc958ddc51c1915ddc4ac89b3bcf4c90f
ranalimayadunne <mayadunneranali@gmail.com>
2024-09-05 13:23:50 +0530

    Implement routing

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,10 @@ import React from "react";
 import Welcome from "./routes/Welcome";
 import Login from "./routes/Login";
 import Signup from "./routes/Signup";
+import Dashboard from "./routes/Dashboard";
+import UserGuide from "./routes/UserGuide";
+import Feedback from "./routes/Feedback";
+import EditProfile from "./routes/EditProfile";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
@@ -13,6 +17,10 @@ function App() {
           <Route path="/" element={<Welcome />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/user-guide" element={<UserGuide />} />
+          <Route path="/feedback" element={<Feedback />} />
+          <Route path="/dashboard/edit-profile" element={<EditProfile />} />
         </Routes>
       </Router>
     </>
